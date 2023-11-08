f1a74dc836d84a5697a48bae265010e73d504703
Xing-C <79847143@qq.com>
2023-11-09 01:13:59 +0800

    更新 ForgeKeybindHelper.java

--- forge/src/main/java/com/github/einjerjar/mc/keymapforge/cross/services/ForgeKeybindHelper.java
+++ forge/src/main/java/com/github/einjerjar/mc/keymapforge/cross/services/ForgeKeybindHelper.java
@@ -3,6 +3,7 @@ package com.github.einjerjar.mc.keymapforge.cross.services;
 import com.github.einjerjar.mc.keymap.cross.services.IKeybindHelper;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import org.apache.commons.lang3.ArrayUtils;
 
